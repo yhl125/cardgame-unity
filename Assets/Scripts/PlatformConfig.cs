ac59bcbd4e86bd77574b4a1caf69e8bcc19a202a
Yoonhyoung-Lee <54613623+Yoonhyoung-Lee@users.noreply.github.com>
2022-12-15 08:11:19 +0900

    Change UNITY_EDITOR API_URI

--- Assets/Scripts/PlatformConfig.cs
+++ Assets/Scripts/PlatformConfig.cs
@@ -6,7 +6,7 @@ public class PlatformConfig : MonoBehaviour
     void Awake()
     {
 #if UNITY_EDITOR
-        Environment.SetEnvironmentVariable("API_URI", "http://localhost:8000");
+        Environment.SetEnvironmentVariable("API_URI", "https://j7s0ihgvz4.execute-api.ap-northeast-2.amazonaws.com");
 
 #elif UNITY_IOS
         Environment.SetEnvironmentVariable("API_URI", "http://localhost:8000");
